6bb20626a5d90c4e11a39aff4110ef156d7d3261
doctorevgesha <gureev.e.98@mail.ru>
2016-04-25 16:07:23 +0300

    Added files via upload

---
+++ telegramkhuibot.php
@@ -0,0 +1,22 @@
+<?
+$access_token = '158309594:AAEP2SaNrEFHRXCP5yfY4zv2vHQA51Dfcvo';
+$api = 'https://api.telegram.org/bot' . $access_token;
+
+$output = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id = 80226362;
+$message = $output['message']['text'];
+
+switch($message) {
+case 'q':
+sendMessage($chat_id, 'Hello World!');
+break;
+default:
+sendMessage($chat_id, 'пошёл на хуй');
+break;
+}
+
+function sendMessage($chat_id, $message) {
+file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+}
+
+?>
